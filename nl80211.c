aa9c3ee716929d75852a11640362a09bb32c5262
John Crispin <john@phrozen.org>
2022-03-11 07:47:49 +0100

    set the AP bssid inside the assoc list
    Signed-off-by: John Crispin <john@phrozen.org>

--- nl80211.c
+++ nl80211.c
@@ -1037,7 +1037,7 @@ int dump_station(struct ubus_context *ctx,
 					w = blobmsg_open_array(&b, wif->name);
 
 				s = blobmsg_open_table(&b, NULL);
-				blobmsg_add_string(&b, "bssid", sta->saddr);
+				blobmsg_add_string(&b, "bssid", wif->saddr);
 				blobmsg_add_string(&b, "station", sta->saddr);
 				if (sta->connected)
 					blobmsg_add_u32(&b, "connected", sta->connected);
